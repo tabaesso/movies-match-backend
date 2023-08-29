6bb3693f0f1bcb819922f05d7ba88f78088cf2ac
tabaesso <tatabaesso@gmail.com>
2023-08-29 20:06:14 -0300

    Add movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [UsersModule, SessionsModule],
+  imports: [UsersModule, SessionsModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
