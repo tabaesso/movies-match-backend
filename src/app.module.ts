e2d1fac3b608c17010ae5aa186ac67664934aff1
tabaesso <tatabaesso@gmail.com>
2023-08-29 20:05:27 -0300

    Add sessions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, SessionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
