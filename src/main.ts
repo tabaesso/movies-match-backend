f1ee163f8b1d2af46b2f3dbebc64d291973307b1
tabaesso <tatabaesso@gmail.com>
2023-09-10 17:46:58 -0300

    Fix cors issue to run locally

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Fix CORS - .env
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['POST', 'PATCH', 'DELETE', 'GET'],
+  });
   await app.listen(3000);
 }
 bootstrap();
