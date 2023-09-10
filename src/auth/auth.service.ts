3cdf91f2e9ff15469a96e821a4ef923d94224f63
tabaesso <tatabaesso@gmail.com>
2023-09-10 12:20:27 -0300

    Add user to auth method response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
 
 @Injectable()
 export class AuthService {
@@ -18,9 +19,11 @@ export class AuthService {
     if (!isMatch) {
       throw new UnauthorizedException();
     }
+
     const payload = { sub: user.id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
+      user: _.omit(user, ['password']),
     };
   }
 }
