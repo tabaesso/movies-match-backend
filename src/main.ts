126eb1d02322a95dc7ec982039973dc5b5e63ec0
tabaesso <tatabaesso@gmail.com>
2023-10-07 22:43:21 -0300

    FIx cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 
   // Fix CORS - .env
   app.enableCors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'https://www.tabaesso.com',
+    ],
     methods: ['POST', 'PATCH', 'DELETE', 'GET'],
   });
   await app.listen(3000);
